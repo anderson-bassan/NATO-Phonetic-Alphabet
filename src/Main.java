c5b5c5ea22803d96e7479b39127b2b8beeadd1e9
anderson-bassan <bassan.programador@gmail.com>
2022-08-05 20:40:40 -0300

    optimize while play loop and make attributes private

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
-    private static boolean play = true;
-
     public static void main(String args[]) {
         Nato nato = new Nato();
 
-        while (play) {
-            play = nato.play();
-        }
+        while (nato.play());
     }
 }
--- src/Nato.java
+++ src/Nato.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Nato {
-    String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
-    String[] nato_alphabet = {"alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliett", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"};
+    private String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+    private String[] nato_alphabet = {"alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliett", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"};
 
-    int random_index;
-    int points;
+    private int random_index;
+    private int points;
 
     public int get_nato_index(String word) {
         for (int i = 0; i < nato_alphabet.length; i++) if (nato_alphabet[i].equals(word)) return i;
