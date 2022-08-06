ae383d21415398bef7bd7f7d0c85d3c7693e4d67
anderson-bassan <bassan.programador@gmail.com>
2022-08-06 17:12:09 -0300

    change variable and methods name to camel case

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Nato nato = new Nato();
 
         while (nato.play());
--- src/Nato.java
+++ src/Nato.java
@@ -1,69 +1,69 @@
 import java.util.Scanner;
 
 public class Nato {
-    private String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
-    private String[] nato_alphabet = {"alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliett", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"};
+    private final String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+    private final String[] natoAlphabet = {"alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel", "india", "juliett", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "xray", "yankee", "zulu", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"};
 
-    private int random_index;
+    private int randomIndex;
     private int points;
 
-    private int get_nato_index(String word) {
-        for (int i = 0; i < nato_alphabet.length; i++) {
-            if (nato_alphabet[i].equals(word)) {
+    private int getNatoIndex(String word) {
+        for (int i = 0; i < natoAlphabet.length; i++) {
+            if (natoAlphabet[i].equals(word)) {
                 return i;
             }
         }
         return -1;
     }
 
-    private String get_input_word() {
-        Scanner input_word = new Scanner(System.in);
-        return input_word.nextLine().toLowerCase();
+    private String getInputWord() {
+        Scanner inputWord = new Scanner(System.in);
+        return inputWord.nextLine().toLowerCase();
     }
 
-    private void set_random_index() {
-        random_index = (int) (Math.random() * alphabet.length);
+    private void setRandomIndex() {
+        randomIndex = (int) (Math.random() * alphabet.length);
     }
-    private int get_random_index() {
-        return random_index;
+    private int getRandomIndex() {
+        return randomIndex;
     }
 
     private void randomize() {
-        set_random_index();
+        setRandomIndex();
     }
 
-    private String get_random_letter() {
-        return alphabet[get_random_index()];
+    private String getRandomLetter() {
+        return alphabet[getRandomIndex()];
     }
 
-    private void show_random_letter() {
-        System.out.printf("What is the word for the symbol %s (to exit type exit): ", get_random_letter());
+    private void showRandomLetter() {
+        System.out.printf("What is the word for the symbol %s (to exit type exit): ", getRandomLetter());
     }
 
-    private void show_current_points() {
+    private void showCurrentPoints() {
         System.out.printf("Your current points are: %d%n%n", points);
     }
 
-    private String get_nato_word() {
-        return nato_alphabet[random_index];
+    private String getNatoWord() {
+        return natoAlphabet[randomIndex];
     }
 
     public boolean play() {
         randomize();
-        show_random_letter();
-        String input_word = get_input_word();
+        showRandomLetter();
+        String inputWord = getInputWord();
 
-        if (input_word.equals("exit")) return false;
+        if (inputWord.equals("exit")) return false;
 
-        if (get_nato_index(input_word) == get_random_index()) {
+        if (getNatoIndex(inputWord) == getRandomIndex()) {
             System.out.printf("%nHooray!%n");
             points++;
         } else {
-            System.out.printf("%nThat's wrong. The correct answer is %s.%n", get_nato_word());
+            System.out.printf("%nThat's wrong. The correct answer is %s.%n", getNatoWord());
             points--;
         }
 
-        show_current_points();
+        showCurrentPoints();
         return true;
     }
 }
