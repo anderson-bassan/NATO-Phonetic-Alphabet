dcac18664ea683c8b056df3542f6a8f83f04b1f4
anderson-bassan <bassan.programador@gmail.com>
2022-08-15 00:30:12 -0300

    implement the 2nd of 4 conditions of the piecewise function and expand the tests

--- src/LevenshteinAlgorithm.java
+++ src/LevenshteinAlgorithm.java
@@ -3,8 +3,19 @@ public class LevenshteinAlgorithm {
 		if (Math.min(stringOne.length(), stringTwo.length()) == 0) {
 			return Math.max(stringOne.length(), stringTwo.length());
 		}
-		
-		return 0;
+
+/* 		if (stringOne.charAt(0) == stringTwo.charAt(0)) {
+			System.out.println(calculateDistance(stringOne.substring(0, stringOne.length() - 1), stringTwo.substring(0, stringTwo.length() - 1)));
+			return 0;
+		} */
+ 		if (stringOne.charAt(stringOne.length() - 1) == stringTwo.charAt(stringTwo.length() - 1)) {
+			// remove last chars from both strings and return a new calculateDistance call
+			return calculateDistance(stringOne.substring(0, stringOne.length() - 1), stringTwo.substring(0, stringTwo.length() - 1));
+			
+		} else {
+			// if characters are different then add one to distance
+			return (calculateDistance(stringOne.substring(0, stringOne.length() - 1), stringTwo.substring(0, stringTwo.length() - 1)) + 1);
+		} 		
 	}
 
 /* 	public boolean fuzzyMatching(String stringOne, String stringTwo, int maximumAcceptedDistance) {
@@ -32,6 +43,32 @@ public class LevenshteinAlgorithm {
 			System.out.println("\ttest two: passed");
 		} else {
 			System.out.println("\ttest two: failed");
+		}
+
+		if (calculateDistance("t", "t") == 0) {
+			System.out.println("\ttest three: passed");
+		} else {
+			System.out.println("\ttest three: failed");
+		}
+		
+		if (calculateDistance("test", "test") == 0) {
+			System.out.println("\ttest four: passed");
+		} else {
+			System.out.println("\ttest four: failed");
+		}
+		
+		if (calculateDistance("test", "est") == 1) {
+			System.out.println("\ttest five: passed");
+		} else {
+			System.out.println("\ttest five: failed");
+			System.out.printf("Expected result: 1%nResult: %s", calculateDistance("test", "est"));
+		}
+
+		if (calculateDistance("test", "tesd") == 1) {
+			System.out.println("\ttest six: passed");
+		} else {
+			System.out.println("\ttest six: failed");
+			System.out.printf("Expected result: 1%nResult: %s", calculateDistance("test", "tesd"));
 		}
 	}
 }
