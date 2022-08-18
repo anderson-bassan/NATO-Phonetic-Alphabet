76f45fb4425286430a1631fcc8e72a5b0b637257
anderson-bassan <bassan.programador@gmail.com>
2022-08-18 16:18:28 -0300

    implements Junit testing in LevenshteinAlgorithm.calculateDistance and removes old tests from LevenshteinAlgorithm

--- src/test/java/com/nato/TestLevenshteinAlgorithm.java
+++ src/test/java/com/nato/TestLevenshteinAlgorithm.java
@@ -6,24 +6,27 @@ import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestLevenshteinAlgorithm {
-	@Test
-	@DisplayName("gato and nothing should be equal to four")
-	void gatoAndNothingShouldBeEqualToFour() {
-		var lev = new LevenshteinAlgorithm();
-		assertEquals(4, lev.calculateDistance("gato", ""));
-	}
 	
 	@Test
-	@DisplayName("nohting and nothing should be equal to zero")
-	void nothingAndNothingShouldBeEqualToZero() {
+	void assertThatCalculateDistanceIsWorkingProperly() {
 		var lev = new LevenshteinAlgorithm();
-		assertEquals(0, lev.calculateDistance("", ""));		
-	}
 
-	@Test
-	@DisplayName("nohting and nothing should be equal to zero")
-	void nothingAndNothingShouldBeEqualToZero() {
-		var lev = new LevenshteinAlgorithm();
-		assertEquals(0, lev.calculateDistance("", ""));		
+		assertAll("calculateDistance",
+			() -> assertEquals(4, lev.calculateDistance("gato", ""), "\"\" and gat should be equal to 0"),
+			() -> assertEquals(0, lev.calculateDistance("", ""), "\"\" and \"\" should be equal to 0"),
+			() -> assertEquals(0, lev.calculateDistance("t", "t"), "t and t should be equal to 0"),
+			() -> assertEquals(0, lev.calculateDistance("test", "test"), "test and test should be equal to 0"),
+			() -> assertEquals(1, lev.calculateDistance("test", "est"), "test and est should be equal to 1"),
+			() -> assertEquals(1, lev.calculateDistance("test", "tesd"), "test and tesd should be equal to 1"),
+			() -> assertEquals(1, lev.calculateDistance("test", "testd"), "test and testd should be equal to 1"),
+			() -> assertEquals(1, lev.calculateDistance("thing", "think"), "test and tesd should be equal to 1"),
+			() -> assertEquals(1, lev.calculateDistance("test", "tesd"), "test and tesd should be equal to 1"),
+			() -> assertEquals(1, lev.calculateDistance("pato", "gato"), "pato and gato should be equal to 1"),
+			() -> assertEquals(2, lev.calculateDistance("back", "book"), "pato and gato should be equal to 2"),
+			() -> assertEquals(6, lev.calculateDistance("strip", "number"), "strip and number should be equal to 6"),
+			() -> assertThrows(NullPointerException.class, () -> lev.calculateDistance("strip", null), "strip and null should throw NullPointerException"),
+			() -> assertThrows(NullPointerException.class, () -> lev.calculateDistance(null, "number"), "null and number should throw NullPointerException"),
+			() -> assertThrows(NullPointerException.class, () -> lev.calculateDistance(null, null), "null and null should throw NullPointerException")
+		);
 	}
 }
